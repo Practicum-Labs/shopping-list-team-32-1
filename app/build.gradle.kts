dffe947db99f44544ad4738fb69621c0ce0f82bb
DaniilKvasnikov <123yk123@mail.ru>
2026-07-12 10:02:09 +0300

    убрать отдельную настройку detekt из app-модуля

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.detekt)
-}
-
-detekt {
-    toolVersion = "1.23.6"
-    config.setFrom(files("${project.rootDir}/config/detekt/detekt.yml"))
-    buildUponDefaultConfig = true
-    allRules = false
 }
 
 android {
