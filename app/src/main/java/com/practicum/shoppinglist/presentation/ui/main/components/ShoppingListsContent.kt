ca8516885d51f8c65f456d338dba85b8759b5697
DaniilKvasnikov <123yk123@mail.ru>
2026-07-12 11:40:11 +0300

    упрощение SwipeableListItem

--- app/src/main/java/com/practicum/shoppinglist/presentation/ui/main/components/ShoppingListsContent.kt
+++ app/src/main/java/com/practicum/shoppinglist/presentation/ui/main/components/ShoppingListsContent.kt
@@ -1,12 +1,7 @@
 package com.practicum.shoppinglist.presentation.ui.main.components
 
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -15,7 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -38,29 +32,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.practicum.shoppinglist.R
 import com.practicum.shoppinglist.domain.model.ShoppingList
 import com.practicum.shoppinglist.presentation.theme.Dimens
 import com.practicum.shoppinglist.presentation.theme.colors
 import com.practicum.shoppinglist.presentation.ui.main.shoppingListIconByName
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 @Composable
 fun ShoppingListsContent(
@@ -222,104 +206,6 @@ fun ShoppingListsContent(
                     }
                 )
             }
-        }
-    }
-}
-
-@Composable
-@Suppress("MagicNumber", "LongParameterList")
-fun SwipeableListItem(
-    onDelete: () -> Unit,
-    backgroundContent: @Composable (isLongSwipe: Boolean, closeItem: () -> Unit) -> Unit,
-    content: @Composable () -> Unit,
-    itemKey: Any,
-    openedItemKey: Any?,
-    onOpenedChange: (Any?) -> Unit,
-    modifier: Modifier = Modifier,
-    actionsWidth: Dp = Dimens.Main.swipeActionsWidth,
-) {
-    val density = LocalDensity.current
-    val actionsWidthPx = with(density) { -actionsWidth.toPx() }
-
-    var offsetX by remember { mutableFloatStateOf(0f) }
-
-    val coroutineScope = rememberCoroutineScope()
-
-    val isLongSwipe = offsetX < actionsWidthPx * 1.5f
-
-    fun animateOffsetTo(target: Float) {
-        coroutineScope.launch {
-            androidx.compose.animation.core.animate(
-                initialValue = offsetX,
-                targetValue = target,
-                animationSpec = spring(stiffness = Spring.StiffnessMediumLow)
-            ) { value, _ ->
-                offsetX = value
-            }
-        }
-    }
-
-    val closeItem: () -> Unit = {
-        if (openedItemKey == itemKey) onOpenedChange(null)
-        animateOffsetTo(0f)
-    }
-
-    LaunchedEffect(openedItemKey) {
-        if (openedItemKey != itemKey && offsetX != 0f) {
-            animateOffsetTo(0f)
-        }
-    }
-
-    Box(
-        modifier = modifier
-            .fillMaxWidth()
-            .pointerInput(itemKey, openedItemKey) {
-                awaitEachGesture {
-                    awaitFirstDown(pass = PointerEventPass.Initial)
-                    if (openedItemKey != null && openedItemKey != itemKey) {
-                        onOpenedChange(null)
-                    }
-                }
-            }
-    ) {
-        Box(
-            modifier = Modifier
-                .matchParentSize()
-                .clip(RoundedCornerShape(Dimens.Main.listItemCornerRadius))
-        ) {
-            backgroundContent(isLongSwipe, closeItem)
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .offset { IntOffset(offsetX.roundToInt(), 0) }
-                .pointerInput(Unit) {
-                    detectHorizontalDragGestures(
-                        onDragEnd = {
-                            val targetOffset = if (offsetX < actionsWidthPx * 1.5f) {
-                                onDelete()
-                                0f
-                            } else if (offsetX < actionsWidthPx / 2f) {
-                                actionsWidthPx
-                            } else {
-                                0f
-                            }
-                            if (targetOffset == 0f) {
-                                if (openedItemKey == itemKey) onOpenedChange(null)
-                            } else {
-                                onOpenedChange(itemKey)
-                            }
-                            animateOffsetTo(targetOffset)
-                        },
-                        onHorizontalDrag = { change, dragAmount ->
-                            change.consume()
-                            offsetX = (offsetX + dragAmount).coerceAtMost(0f)
-                        }
-                    )
-                }
-        ) {
-            content()
         }
     }
 }
